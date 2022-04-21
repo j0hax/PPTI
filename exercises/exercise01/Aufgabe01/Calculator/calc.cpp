2f8080333665ee1490eea8a942ba916ed1381e7e
Markus Olbrich <markus.olbrich@gebil.de>
2022-04-21 11:34:23 +0200

    Lecture 1

---
+++ exercises/exercise01/Aufgabe01/Calculator/calc.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+    int numberA;
+    int numberB;
+    std::string operation;    
+
+    // TODO: Add code here
+
+    return 0;
+}
+
+
---
+++ exercises/exercise01/Aufgabe01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+void main()
+{
+    String name;
+
+    cout << "What is your given name? '
+    std::cin << name;
+    
+    for(Int count = 0; count = 10; ++count)
+        std::cout<< "Hello " << name "\n";
+}
+
