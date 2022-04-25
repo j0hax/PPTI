6bc8e439f6a0c037440e756dd56a652bbb4c7b29
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-04-25 21:32:35 +0200

    Teilaufgabe 4

--- exercises/exercise01/Aufgabe01/Calculator/calc.cpp
+++ exercises/exercise01/Aufgabe01/Calculator/calc.cpp
@@ -1,15 +1,32 @@
-#include <string>
 #include <iostream>
+#include <string>
 
-int main()
-{
-    int numberA;
-    int numberB;
-    std::string operation;    
+int main() {
+  int numberA;
+  int numberB;
+  std::string operation;
 
-    // TODO: Add code here
+  std::cout << "Number1: ";
+  std::cin >> numberA;
 
-    return 0;
+  std::cout << "Number2: ";
+  std::cin >> numberB;
+
+  std::cout << "Operation: ";
+  std::cin >> operation;
+
+  int result;
+
+  if (operation == "add") {
+    result = numberA + numberB;
+  } else if (operation == "sub") {
+    result = numberA - numberB;
+  } else {
+    std::cerr << "Operation not supported!\n";
+    return 1;
+  }
+
+  std::cout << result << std::endl;
+
+  return 0;
 }
-
-
