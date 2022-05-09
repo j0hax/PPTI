9868d545adad92350a35b493c7034b2a9c375ae6
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-05-09 20:21:55 +0200

    Implement newwc

--- exercises/exercise03/newWc/src/newwc.cpp
+++ exercises/exercise03/newWc/src/newwc.cpp
@@ -4,48 +4,60 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> split(std::istream& is, char delim)
-{
-    std::vector<std::string> result;
-    std::string elem;
+std::vector<std::string> split(std::istream& is, char delim) {
+  std::vector<std::string> result;
+  std::string elem;
 
-    while(std::getline(is, elem, delim))
-    {
-        if(elem != "")
-            result.push_back(std::move(elem));
-    }
-    return result;
+  while (std::getline(is, elem, delim)) {
+    if (elem != "")
+      result.push_back(std::move(elem));
+  }
+  return result;
 }
 
-std::vector<std::string> split(const std::string& val, char delim)
-{
-    std::stringstream is(val);
-    return split(is, delim);
+std::vector<std::string> split(const std::string& val, char delim) {
+  std::stringstream is(val);
+  return split(is, delim);
 }
 
-struct WCResult 
-{
-    std::string Name;
-    std::size_t LineCount;
-    std::size_t WordCount;
-    std::size_t ByteCount;
-    
-    WCResult(const std::string& name = "", std::size_t lc = 0, std::size_t wc = 0, std::size_t bc = 0): Name(name), LineCount(lc), WordCount(wc), ByteCount(bc) 
-    {}
-    void output(std::ostream& os) 
-    { 
-        os << this->LineCount << " " << this->WordCount << " " << this->ByteCount << " " << this->Name << std::endl; 
-    }
+struct WCResult {
+  std::string Name;
+  std::size_t LineCount;
+  std::size_t WordCount;
+  std::size_t ByteCount;
+
+  WCResult(const std::string& name = "",
+           std::size_t lc = 0,
+           std::size_t wc = 0,
+           std::size_t bc = 0)
+      : Name(name), LineCount(lc), WordCount(wc), ByteCount(bc) {}
+  void output(std::ostream& os) {
+    os << this->LineCount << " " << this->WordCount << " " << this->ByteCount
+       << " " << this->Name << std::endl;
+  }
 };
 
-//TODO: Implement getWordCount her
+WCResult getWordCount(std::istream& input, const std::string& name) {
+  // Put file contents into a buffer to avoid constantly changing the current
+  // character in the stream
+  std::stringstream buf;
+  buf << input.rdbuf();
 
-int main(int argc, char** argv)
-{
-    std::vector<std::string> args(argv, argv + argc);
-    
-    //TODO: Implement functionality
-    
-    return 0;
+  std::vector<std::string> lines = split(buf.str(), '\n');
+  std::vector<std::string> words = split(buf.str(), ' ');
+
+  return WCResult(name, lines.size(), words.size(), buf.str().size());
 }
 
+int main(int argc, char** argv) {
+  std::vector<std::string> args(argv, argv + argc);
+
+  std::ifstream ifs;
+  for (size_t i = 1; i < args.size(); i++) {
+    ifs.open(args[i]);
+    getWordCount(ifs, args[i]).output(std::cout);
+    ifs.close();
+  }
+
+  return 0;
+}
