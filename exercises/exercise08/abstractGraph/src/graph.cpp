a26e2a279e88a6e117e9fa5c57a03b80201d56a2
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-06-26 20:44:40 +0200

    Implement Aufgabe 08

--- exercises/exercise08/abstractGraph/src/graph.cpp
+++ exercises/exercise08/abstractGraph/src/graph.cpp
@@ -1,24 +1,169 @@
-#include <cmath>
-#include <tuple>
-#include <iostream>
 #include "graph.h"
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+#include <tuple>
+#include <vector>
 
-std::ostream& operator<<(std::ostream& os, const Graph& graph)
-{
-    os << "Nodes : \n";
-    graph.forAllNodes([&](std::size_t nodeId)
-        {
-            os<<nodeId<<'\n';
-        });
-    os << "Edges : \n";
-    graph.forAllEdges([&](std::size_t edgeId)
-        {
-            auto [from, to] = graph.getAdjacentNodeIds(edgeId);
-            os<<edgeId<<" : "<<from<<"->( "<<graph.getEdgeWeight(edgeId)<<" )->"<<to<<'\n';
-        });
-    os.flush();
-    return os;
+std::ostream& operator<<(std::ostream& os, const Graph& graph) {
+  os << "Nodes : \n";
+  graph.forAllNodes([&](std::size_t nodeId) { os << nodeId << '\n'; });
+  os << "Edges : \n";
+  graph.forAllEdges([&](std::size_t edgeId) {
+    auto [from, to] = graph.getAdjacentNodeIds(edgeId);
+    os << edgeId << " : " << from << "->( " << graph.getEdgeWeight(edgeId)
+       << " )->" << to << '\n';
+  });
+  os.flush();
+  return os;
 }
 
-// TODO: Implement member functions of AdjacentGraph and ClassicGraph
+// Implementation for AdjacentGraph
+std::size_t AdjacentGraph::addEdge(std::size_t nodeId1,
+                                   std::size_t nodeId2,
+                                   double weight) {
+  this->weights[nodeId1][nodeId2] = weight;
 
+  // formula for finding edge: y * len(matrix) + x
+  return nodeId1 * this->weights.size() + nodeId2;
+}
+
+std::size_t AdjacentGraph::addNode() {
+  // Add another vector to the vector
+  const size_t s = this->weights.size() + 1;
+
+  weights.resize(s);
+
+  // update each row
+  for (auto& row : this->weights) {
+    row.resize(s);
+  }
+
+  return s;
+}
+
+std::tuple<std::size_t, std::size_t> AdjacentGraph::getAdjacentNodeIds(
+    std::size_t edgeId) const {
+  // Get "coordinates" from the ID
+  size_t r = 0;
+  while ((r+1) * this->weights.size() <= edgeId) {
+    r += 1;
+  }
+  size_t c = edgeId - (r * this->weights.size());
+
+  return std::make_tuple(c, r);
+}
+
+std::vector<std::size_t> AdjacentGraph::getInEdgeIds(std::size_t nodeId) const {
+  std::vector<std::size_t> refs;
+  // Loop over specified column and add positive values
+  for (std::size_t i = 0; i < this->weights.size(); ++i) {
+    if (this->weights[i][nodeId] > 0) {
+      refs.push_back(i);
+    }
+  }
+
+  return refs;
+}
+
+std::vector<std::size_t> AdjacentGraph::getOutEdgeIds(
+    std::size_t nodeId) const {
+  std::vector<std::size_t> refs;
+  // Loop over specified row and add positive values
+  for (std::size_t i = 0; i < this->weights.size(); ++i) {
+    if (this->weights[nodeId][i] > 0) {
+      refs.push_back(i);
+    }
+  }
+
+  return refs;
+}
+
+void AdjacentGraph::setEdgeWeight(std::size_t edgeId, double weight) {
+  std::tuple<std::size_t, std::size_t> coords =
+      this->getAdjacentNodeIds(edgeId);
+  this->weights[std::get<0>(coords)][std::get<1>(coords)] = weight;
+}
+
+double AdjacentGraph::getEdgeWeight(std::size_t edgeId) const {
+  std::tuple<std::size_t, std::size_t> coords =
+      this->getAdjacentNodeIds(edgeId);
+  return this->weights[std::get<0>(coords)][std::get<1>(coords)];
+}
+
+void AdjacentGraph::forAllEdges(
+    const std::function<void(std::size_t edgeId)>& function) const {
+  for (size_t r = 0; r < this->weights.size(); ++r) {
+    for (size_t c = 0; c < this->weights.size(); ++c) {
+      if (this->weights[r][c] > 0) {
+        // calculate edge id
+        size_t edge = r * this->weights.size() + c;
+        function(edge);
+      }
+    }
+  }
+}
+
+void AdjacentGraph::forAllNodes(
+    const std::function<void(std::size_t nodeId)>& function) const {
+  for (size_t r = 0; r < this->weights.size(); ++r) {
+    function(r);
+  }
+}
+
+// Implementation for ClassicGraph
+std::size_t ClassicGraph::addEdge(std::size_t nodeId1,
+                                  std::size_t nodeId2,
+                                  double weight) {
+  ClassicGraph::Edge e;
+  e.node1 = nodeId1;
+  e.node2 = nodeId2;
+  e.weight = weight;
+  this->edges.push_back(e);
+  return this->edges.size();
+}
+
+std::size_t ClassicGraph::addNode() {
+  ClassicGraph::Node n;
+  this->nodes.push_back(n);
+  return this->nodes.size();
+}
+
+std::tuple<std::size_t, std::size_t> ClassicGraph::getAdjacentNodeIds(
+    std::size_t edgeId) const {
+  ClassicGraph::Edge e = this->edges[edgeId];
+  return std::make_tuple(e.node1, e.node2);
+}
+
+std::vector<std::size_t> ClassicGraph::getInEdgeIds(std::size_t nodeId) const {
+  ClassicGraph::Node n = this->nodes[nodeId];
+  return n.incoming;
+}
+
+std::vector<std::size_t> ClassicGraph::getOutEdgeIds(std::size_t nodeId) const {
+  ClassicGraph::Node n = this->nodes[nodeId];
+  return n.outgoing;
+}
+
+void ClassicGraph::setEdgeWeight(std::size_t edgeId, double weight) {
+  this->edges[edgeId].weight = weight;
+}
+
+double ClassicGraph::getEdgeWeight(std::size_t edgeId) const {
+  ClassicGraph::Edge e = this->edges[edgeId];
+  return e.weight;
+}
+
+void ClassicGraph::forAllEdges(
+    const std::function<void(std::size_t edgeId)>& function) const {
+  for (size_t i = 0; i < this->edges.size(); ++i) {
+    function(i);
+  }
+}
+
+void ClassicGraph::forAllNodes(
+    const std::function<void(std::size_t nodeId)>& function) const {
+  for (size_t i = 0; i < this->edges.size(); ++i) {
+    function(i);
+  }
+}
--- exercises/exercise08/abstractGraph/src/graph.h
+++ exercises/exercise08/abstractGraph/src/graph.h
@@ -1,74 +1,128 @@
 #ifndef ABSTRACTGRAPH_GRAPH_H
 #define ABSTRACTGRAPH_GRAPH_H
+#include <cmath>
 #include <cstddef>
 #include <functional>
+#include <iostream>
 #include <map>
 #include <memory>
-#include <iostream>
-#include <cmath>
 #include <tuple>
+#include <vector>
 
 /**
  * Graph is an abstract base class for specific graph implementations
  */
-class Graph 
-{
-public:
-    /// Virtual destructor (necessary due to abstract base class usage)
-    virtual ~Graph() = default;
+class Graph {
+ public:
+  /// Virtual destructor (necessary due to abstract base class usage)
+  virtual ~Graph() = default;
 
-    /// Inserts a directed Edge into the Graph if the Ids of both Nodes are valid within the Graph
-    /// @param nodeId1 Id of incoming Node from which the Edge originates
-    /// @param nodeId2 Id of outgoing Node to which the Edge leads
-    /// @param weight Weight of the Edge
-    /// @returns Id of the new Edge
-    virtual std::size_t addEdge(std::size_t nodeId1, std::size_t nodeId2, double weight) = 0;
+  /// Inserts a directed Edge into the Graph if the Ids of both Nodes are valid
+  /// within the Graph
+  /// @param nodeId1 Id of incoming Node from which the Edge originates
+  /// @param nodeId2 Id of outgoing Node to which the Edge leads
+  /// @param weight Weight of the Edge
+  /// @returns Id of the new Edge
+  virtual std::size_t addEdge(std::size_t nodeId1,
+                              std::size_t nodeId2,
+                              double weight) = 0;
 
-    /// Adds a new Node to the Graph and returns its Id
-    /// @returns Id of the new Node
-    virtual std::size_t addNode() = 0;
+  /// Adds a new Node to the Graph and returns its Id
+  /// @returns Id of the new Node
+  virtual std::size_t addNode() = 0;
 
-    /// Returns a tuple of the two Nodes connected by an Edge
-    /// @param edgeId Id of the Edge
-    /// @returns Tuple of the Ids of the two Nodes connected by the given Edge
-    virtual std::tuple<std::size_t,std::size_t> getAdjacentNodeIds(std::size_t edgeId) const = 0;
+  /// Returns a tuple of the two Nodes connected by an Edge
+  /// @param edgeId Id of the Edge
+  /// @returns Tuple of the Ids of the two Nodes connected by the given Edge
+  virtual std::tuple<std::size_t, std::size_t> getAdjacentNodeIds(
+      std::size_t edgeId) const = 0;
 
-    /// Returns Ids of all Edges going into a Node
-    /// @param nodeId Id of the Node
-    /// @returns Vector of Ids of incoming Edges
-    virtual std::vector<std::size_t> getInEdgeIds(std::size_t nodeId) const = 0;
+  /// Returns Ids of all Edges going into a Node
+  /// @param nodeId Id of the Node
+  /// @returns Vector of Ids of incoming Edges
+  virtual std::vector<std::size_t> getInEdgeIds(std::size_t nodeId) const = 0;
 
-    /// Returns Ids of all Edges coming out of a Node
-    /// @param nodeId Id of the Node
-    /// @returns Vector of Ids of outgoing Edges
-    virtual std::vector<std::size_t> getOutEdgeIds(std::size_t nodeId) const = 0;
+  /// Returns Ids of all Edges coming out of a Node
+  /// @param nodeId Id of the Node
+  /// @returns Vector of Ids of outgoing Edges
+  virtual std::vector<std::size_t> getOutEdgeIds(std::size_t nodeId) const = 0;
 
-    /// Replaces the weight of an Edge
-    /// @param edgeId Id of the Edge
-    /// @param weight New weight to be set
-    virtual void setEdgeWeight(std::size_t edgeId, double weight) = 0;
+  /// Replaces the weight of an Edge
+  /// @param edgeId Id of the Edge
+  /// @param weight New weight to be set
+  virtual void setEdgeWeight(std::size_t edgeId, double weight) = 0;
 
-    /// Returns the weight of an Edge
-    /// @param edgeId Id of the Edge
-    virtual double getEdgeWeight(std::size_t edgeId) const = 0;
+  /// Returns the weight of an Edge
+  /// @param edgeId Id of the Edge
+  virtual double getEdgeWeight(std::size_t edgeId) const = 0;
 
-    /// Executes a function on all Edges of the Graph
-    /// @param function The function to be called
-    virtual void forAllEdges(const std::function<void(std::size_t edgeId)>& function) const = 0;
+  /// Executes a function on all Edges of the Graph
+  /// @param function The function to be called
+  virtual void forAllEdges(
+      const std::function<void(std::size_t edgeId)>& function) const = 0;
 
-    /// Executes a function on all Nodes of the Graph
-    /// @param function The function to be called
-    virtual void forAllNodes(const std::function<void(std::size_t nodeId)>& function) const = 0;
+  /// Executes a function on all Nodes of the Graph
+  /// @param function The function to be called
+  virtual void forAllNodes(
+      const std::function<void(std::size_t nodeId)>& function) const = 0;
 
-private:
-    friend std::ostream & operator<<(std::ostream &os, const Graph& g);
+ private:
+  friend std::ostream& operator<<(std::ostream& os, const Graph& g);
 };
 
-// TODO: Implement class AdjacentGraph
+class AdjacentGraph : public Graph {
+ private:
+  std::vector<std::vector<double>> weights;
 
-// TODO: Implement class ClassicGraph
+ public:
+  std::size_t addEdge(std::size_t nodeId1, std::size_t nodeId2, double weight);
+  std::size_t addNode();
+  std::tuple<std::size_t, std::size_t> getAdjacentNodeIds(
+      std::size_t edgeId) const;
+  std::vector<std::size_t> getInEdgeIds(std::size_t nodeId) const;
+  std::vector<std::size_t> getOutEdgeIds(std::size_t nodeId) const;
+  void setEdgeWeight(std::size_t edgeId, double weight);
+  double getEdgeWeight(std::size_t edgeId) const;
+  void forAllEdges(
+      const std::function<void(std::size_t edgeId)>& function) const;
+  void forAllNodes(
+      const std::function<void(std::size_t nodeId)>& function) const;
+};
 
-// BONUS TODO: Implement template classes GraphT, AdjacentGraphT and ClassicGraphT
+class ClassicGraph : public Graph {
+ public:
+  std::size_t addEdge(std::size_t nodeId1, std::size_t nodeId2, double weight);
+  std::size_t addNode();
+  std::tuple<std::size_t, std::size_t> getAdjacentNodeIds(
+      std::size_t edgeId) const;
+  std::vector<std::size_t> getInEdgeIds(std::size_t nodeId) const;
+  std::vector<std::size_t> getOutEdgeIds(std::size_t nodeId) const;
+  void setEdgeWeight(std::size_t edgeId, double weight);
+  double getEdgeWeight(std::size_t edgeId) const;
+  void forAllEdges(
+      const std::function<void(std::size_t edgeId)>& function) const;
+  void forAllNodes(
+      const std::function<void(std::size_t nodeId)>& function) const;
 
-#endif //ABSTRACTGRAPH_GRAPH_H
+  class Edge {
+   public:
+    double weight;
+    size_t node1;
+    size_t node2;
+  };
 
+  class Node {
+   public:
+    std::vector<std::size_t> incoming;
+    std::vector<std::size_t> outgoing;
+  };
+
+ private:
+  std::vector<Edge> edges;
+  std::vector<Node> nodes;
+};
+
+// BONUS TODO: Implement template classes GraphT, AdjacentGraphT and
+// ClassicGraphT
+
+#endif  // ABSTRACTGRAPH_GRAPH_H
