e036a31bd123057a4e3233c4abf6e0612484e3f3
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-06-30 21:37:24 +0200

    Implement Teilaufgabe 1

--- exercises/exercise09/hanoi/src/hanoi.cpp
+++ exercises/exercise09/hanoi/src/hanoi.cpp
@@ -4,8 +4,12 @@
 
 #include "hanoi.h"
 #include <algorithm>
+#include <cstddef>
 #include <deque>
+#include <iostream>
+#include <ostream>
 #include <stack>
+#include <string>
 
 std::string& Tower::getName() {
   return this->name;
@@ -38,7 +42,20 @@ void Hanoi::moveTopPlate(
     const std::function<void(const std::string&, const std::string&)>&
         preMovePrint,
     const std::function<void()>& postMovePrint) {
-  // TODO: Implement
+        preMovePrint(origin.getName(), target.getName());
+        
+        // Check if move is legal
+        if (!target.getTower().empty()) {
+        if (target.getTower().back() < origin.getTower().front()) {
+            return;
+        }
+        }
+
+        size_t p = origin.getTower().back();
+        origin.getTower().pop_back();
+        target.getTower().push_back(p);
+
+        postMovePrint();
 }
 
 void Hanoi::solveHanoi(
@@ -49,12 +66,59 @@ void Hanoi::solveHanoi(
     const std::function<void(const std::string&, const std::string&)>&
         preMovePrint,
     const std::function<void()>& postMovePrint) {
-  // TODO: Implement
+        if (towersize == 1) {
+            moveTopPlate(source, destination, preMovePrint, postMovePrint);
+        } else {
+            solveHanoi(towersize - 1, source, spare, destination, preMovePrint, postMovePrint);
+            moveTopPlate(source, destination, preMovePrint, postMovePrint);
+            solveHanoi(towersize - 1, spare, destination, source, preMovePrint, postMovePrint);
+        }
 }
 
 void Hanoi::solveHanoiStart(std::ostream& os,
                             Tower& source,
                             Tower& destination,
                             Tower& spare) {
-  // TODO: Implement
+
+    auto pre = [&](std::string a, std::string b)
+    {
+        os << a << " -> " << b << std::endl;
+    };
+
+    auto post = [&]()
+    {
+
+        // Get the greatest sized tower
+        size_t m = std::max({towerA.getTower().size(), towerB.getTower().size(), towerC.getTower().size()});
+
+        for (int i = m - 1; i >= 0; --i) {
+            if (i < towerA.getTower().size()) {
+                os << towerA.getTower()[i];
+            } else {
+                os << "│";
+            }
+
+            os << " ";
+
+            if (i < towerB.getTower().size()) {
+                os << towerB.getTower()[i];
+            } else {
+                os << "│";
+            }
+
+            os << " ";
+
+            if (i < towerC.getTower().size()) {
+                os << towerC.getTower()[i];
+            } else {
+                os << "│";
+            }
+
+            os << std::endl;
+        }
+
+        os << "╧═╧═╧" << std::endl;
+    };
+
+    solveHanoi(this->towerA.getTower().size(), source, destination, spare, pre, post);
 }
