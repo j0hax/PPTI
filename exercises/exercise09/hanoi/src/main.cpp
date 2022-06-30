31225891cc86d6dfa1ebf91d29d5bce0f23754ec
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-06-30 20:09:47 +0200

    Format files

--- exercises/exercise09/hanoi/src/main.cpp
+++ exercises/exercise09/hanoi/src/main.cpp
@@ -1,24 +1,22 @@
 /**
- * Main file. 
+ * Main file.
  */
 
-#include <vector> 
 #include <iostream>
+#include <vector>
 #include "hanoi.h"
-    
+
 /**
  * Main test function
  */
-int main()
-{
-    std::cout << "First Example: Tower of height 3  A->B" << std::endl; 
-    Hanoi hanoi(3);
-    
+int main() {
+  std::cout << "First Example: Tower of height 3  A->B" << std::endl;
+  Hanoi hanoi(3);
+
 #if false
     std::cout << "\n\nSecond Example: Tower of height 5\n";
-    Hanoi hanoi2(5); 
+    Hanoi hanoi2(5);
 #endif
 
-    return 0;
+  return 0;
 }
-
--- exercises/exercise09/sieve/src/sieve.cpp
+++ exercises/exercise09/sieve/src/sieve.cpp
@@ -1,34 +1,26 @@
 #include "sieve.h"
 #include <algorithm>
-#include <vector>
 #include <iostream>
+#include <vector>
 
 Sieve::Sieve(std::vector<std::size_t>& numbers) : numbers(numbers) {}
 
-void Sieve::reduceToPrimes()
-{
-    // TODO: Implement reduceToPrimes
+void Sieve::reduceToPrimes() {
+  // TODO: Implement reduceToPrimes
 }
 
-void Sieve::resetToSize(std::size_t size)
-{
-    // TODO: Implement resetToSize    
+void Sieve::resetToSize(std::size_t size) {
+  // TODO: Implement resetToSize
 }
 
-void Sieve::groupNumbers(std::size_t lowerBound, std::size_t upperBound)
-{
-    // TODO: Implement groupNumbers
+void Sieve::groupNumbers(std::size_t lowerBound, std::size_t upperBound) {
+  // TODO: Implement groupNumbers
 }
 
-void Sieve::printNumbers(std::ostream& os)
-{
-    os << "{";
-    for (std::size_t i = 0; i < this->numbers.size() - 1; ++i)
-    {
-        os << this->numbers[i] << ", ";
-    }
-    os << this->numbers[this->numbers.size() - 1] << "}\n\n" << std::flush;
+void Sieve::printNumbers(std::ostream& os) {
+  os << "{";
+  for (std::size_t i = 0; i < this->numbers.size() - 1; ++i) {
+    os << this->numbers[i] << ", ";
+  }
+  os << this->numbers[this->numbers.size() - 1] << "}\n\n" << std::flush;
 }
-
-
-
