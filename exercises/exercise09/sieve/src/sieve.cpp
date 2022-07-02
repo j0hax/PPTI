9974e2077507ed367f2a2735eefc11d7a4148b8c
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-07-03 01:30:43 +0200

    Implement Teilaufgabe 2

--- exercises/exercise09/sieve/src/sieve.cpp
+++ exercises/exercise09/sieve/src/sieve.cpp
@@ -1,20 +1,51 @@
 #include "sieve.h"
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <vector>
 
 Sieve::Sieve(std::vector<std::size_t>& numbers) : numbers(numbers) {}
 
 void Sieve::reduceToPrimes() {
-  // TODO: Implement reduceToPrimes
+  sort(this->numbers.begin(), this->numbers.end());
+
+  const std::size_t m =
+      *std::max_element(this->numbers.begin(), this->numbers.end());
+  const std::size_t coeff_limit = static_cast<std::size_t>(std::sqrt(m));
+
+  for (std::size_t c = 2; c <= coeff_limit; ++c) {
+    // remove multiples of the coeffecient c
+    std::size_t i = static_cast<std::size_t>(std::pow(c, 2));
+    for (; i <= m; i += c) {
+      this->numbers.erase(
+          std::remove(this->numbers.begin(), this->numbers.end(), i),
+          this->numbers.end());
+    }
+  }
 }
 
 void Sieve::resetToSize(std::size_t size) {
-  // TODO: Implement resetToSize
+  const auto biggerThan = [&size](const auto& i) { return i > size; };
+
+  this->numbers.erase(
+      std::remove_if(this->numbers.begin(), this->numbers.end(), biggerThan),
+      this->numbers.end());
+  // Add the missing elements
+  for (std::size_t i = 2; i <= size; ++i) {
+    if (std::find(this->numbers.begin(), this->numbers.end(), i) ==
+        this->numbers.end()) {
+      this->numbers.push_back(i);
+    }
+  }
+
+  // Sort again, in descending order
+  std::sort(this->numbers.begin(), this->numbers.end(), std::greater<>());
 }
 
 void Sieve::groupNumbers(std::size_t lowerBound, std::size_t upperBound) {
-  // TODO: Implement groupNumbers
+  std::stable_partition(
+      this->numbers.begin(), this->numbers.end(),
+      [&](const auto& n) { return lowerBound < n && n < upperBound; });
 }
 
 void Sieve::printNumbers(std::ostream& os) {
